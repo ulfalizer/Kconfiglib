f8278c4d58d02db34b1110fe978d09d7db372b44
David Corvoysier <david.corvoysier@gmail.com>
2016-10-06 10:38:00 +0200

    source: allow wildcards in path
    When sourcing Kconfig files, wildcards are accepted in the path to match
    multiple paths.
    
    This allows a source tree to be extended without explicitly editing the
    top Kconfig files.
    
    Example:
    
    source "drivers/*/Kconfig"

--- kconfiglib.py
+++ kconfiglib.py
@@ -69,6 +69,7 @@ might add it in a safe way as a client API instead."""
 import os
 import re
 import sys
+import glob
 
 # File layout:
 #
@@ -675,19 +676,20 @@ class Config(object):
             elif t0 == T_SOURCE or t0 == T_MINUS_SOURCE:
                 kconfig_file = tokens.get_next()
                 exp_kconfig_file = self._expand_sym_refs(kconfig_file)
-                f = os.path.join(self.base_dir, exp_kconfig_file)
-                if os.path.exists(f):
-                    # Add items to the same block
-                    self._parse_file(f, parent, deps, visible_if_deps, block)
-                elif t0 == T_SOURCE:
-                    raise IOError('{0}:{1}: sourced file "{2}" (expands to '
-                                  '"{3}") not found. Perhaps base_dir '
-                                  '(argument to Config.__init__(), currently '
-                                  '"{4}") is set to the wrong value.'
-                                  .format(line_feeder.filename,
-                                          line_feeder.linenr,
-                                          kconfig_file, exp_kconfig_file,
-                                          self.base_dir))
+                g = glob.glob(os.path.join(self.base_dir,exp_kconfig_file))
+                for f in g:
+                    if os.path.exists(f):
+                        # Add items to the same block
+                        self._parse_file(f, parent, deps, visible_if_deps, block)
+                    elif t0 == T_SOURCE:
+                        raise IOError('{0}:{1}: sourced file "{2}" (expands to '
+                                      '"{3}") not found. Perhaps base_dir '
+                                      '(argument to Config.__init__(), currently '
+                                      '"{4}") is set to the wrong value.'
+                                     .format(line_feeder.filename,
+                                              line_feeder.linenr,
+                                              kconfig_file, exp_kconfig_file,
+                                             self.base_dir))
 
             elif t0 == end_marker:
                 # We have reached the end of the block
