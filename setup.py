c91cf2edcd20ab29d5693003387a627210fe9f0e
Ulf Magnusson <ulfalizer@gmail.com>
2017-09-23 17:00:36 +0200

    Modernize and update installation files
    Preparation for adding to PyPI. Switch to setuptools and add a bunch
    more metadata. Use semantic versioning (http://semver.org).

--- setup.py
+++ setup.py
@@ -1,17 +1,46 @@
-#!/usr/bin/env python
+import os
+import setuptools
 
-"""Setup script for the kconfiglib module."""
+here_dir = os.path.dirname(__file__)
+with open(os.path.join(here_dir, "README.rst")) as f:
+    long_description = f.read()
 
-from distutils.core import setup
-
-setup (# Distribution meta-data
-       name = "kconfiglib",
-       version = "0.0.1",
-       description = "A flexible Python Kconfig parser",
-       author = "Ulf Magnusson",
-       author_email = "ulfalizer@gmail.com",
-       url = "https://github.com/ulfalizer/Kconfiglib",
-
-       # Description of the modules and packages in the distribution
-       py_modules = ['kconfiglib'],
-      )
+setuptools.setup(
+    name="kconfiglib",
+    # MAJOR.MINOR.MAINTENANCE per http://semver.org
+    version="1.0.0",
+    description="A flexible Python Kconfig parser",
+    long_description=long_description,
+    url="https://github.com/ulfalizer/Kconfiglib",
+    author='Ulf "Ulfalizer" Magnusson',
+    author_email="ulfalizer@gmail.com",
+    keywords="kconfig, kbuild",
+    py_modules=["kconfiglib"],
+    # This python_requires should be correct, but my setuptools is too old to
+    # test it, so play it safe and leave it out for now. It's unlikely that
+    # anyone's running ancient Python versions anyway, and the problem should
+    # be obvious.
+    #
+    # 2.7+ for Python 2, 3.1+ for Python 3 (for unnumbered {} with format())
+    # python_requires=">=2.7,!=3.0.*",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "Topic :: System :: Operating System Kernels :: Linux",
+        "License :: OSI Approved :: ISC License (ISCL)",
+        "Operating System :: POSIX",
+        "Operating System :: Microsoft :: Windows",
+        "Programming Language :: Python :: 2",
+        # Needs support for unnumbered {} in format()
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        # Needs support for unnumbered {} in format()
+        "Programming Language :: Python :: 3.1",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy"])
