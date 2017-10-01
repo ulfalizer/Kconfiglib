80279c463725971430b8f9410040966ed2e2b303
Ulf Magnusson <ulfalizer@gmail.com>
2017-10-01 07:28:19 +0200

    Make 1.0.5 release
    The only fix is 2a2fcbd (Propagate dependencies to range conditions),
    which fixes an obscure issue with dependencies on 'range x y'
    properties.
    
    Also includes some internal cleanup and optimization.

--- README.rst
+++ README.rst
@@ -26,7 +26,7 @@ installed with e.g.
 
     $ pip(3) install kconfiglib --user
 
-All releases have a corresponding tag in the git repository, e.g. ``v1.0.4``.
+All releases have a corresponding tag in the git repository, e.g. ``v1.0.5``.
 `Semantic versioning <http://semver.org/>`_ is used.
 
 Installation for the Linux kernel
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(os.path.join(here_dir, "README.rst")) as f:
 setuptools.setup(
     name="kconfiglib",
     # MAJOR.MINOR.MAINTENANCE per http://semver.org
-    version="1.0.4",
+    version="1.0.5",
     description="A flexible Python Kconfig parser",
     long_description=long_description,
     url="https://github.com/ulfalizer/Kconfiglib",
