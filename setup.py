d7526d2fb5768508941d3fa51a48109e6c0d4029
Ulf Magnusson <ulfalizer@gmail.com>
2017-11-11 13:00:06 +0100

    Make 2.0.2 release
    Just a few nits and a small optimization: d890343 (Micro-optimize
    write_config() a bit). No bug fixes or functional changes.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(os.path.join(here_dir, "README.rst")) as f:
 setuptools.setup(
     name="kconfiglib",
     # MAJOR.MINOR.MAINTENANCE per http://semver.org
-    version="2.0.1",
+    version="2.0.2",
     description="A flexible Python Kconfig parser",
     long_description=long_description,
     url="https://github.com/ulfalizer/Kconfiglib",
