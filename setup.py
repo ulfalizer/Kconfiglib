b419eb3cd2050ce75d5b8a540c5b7359ec5d7455
Ulf Magnusson <ulfalizer@gmail.com>
2019-04-18 10:30:56 +0200

    Make 10.48.0 release
    Improves save/load UI design in the menuconfig, in commit 90c5573
    ("menuconfig: Remember last saved/loaded path and improve
    _conf_changed").

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ import setuptools
 setuptools.setup(
     name="kconfiglib",
     # MAJOR.MINOR.PATCH, per http://semver.org
-    version="10.47.0",
+    version="10.48.0",
     description="A flexible Python Kconfig implementation",
 
     # Make sure that README.rst decodes on Python 3 in environments that use
