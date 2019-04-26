a451293610db47e014226962bd08d4e4e7cb473e
Ulf Magnusson <ulfalizer@gmail.com>
2019-04-26 17:37:56 +0200

    menuconfig: Remove unused UNKNOWN import

--- menuconfig.py
+++ menuconfig.py
@@ -193,7 +193,7 @@ import sys
 import textwrap
 
 from kconfiglib import Symbol, Choice, MENU, COMMENT, MenuNode, \
-                       BOOL, TRISTATE, STRING, INT, HEX, UNKNOWN, \
+                       BOOL, TRISTATE, STRING, INT, HEX, \
                        AND, OR, \
                        expr_str, expr_value, split_expr, \
                        standard_sc_expr_str, \
