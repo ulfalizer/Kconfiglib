544888a059d1c066f00e2f0f174fb4199e349f52
Ulf Magnusson <ulfalizer@gmail.com>
2019-06-01 06:10:52 +0200

    Make 11.9.1 release
    Fixes an obscure rsource crash, in commit 92791a3 ("Fix obscure crash
    with rsource and $srctree pointing to a symlink").

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ import setuptools
 setuptools.setup(
     name="kconfiglib",
     # MAJOR.MINOR.PATCH, per http://semver.org
-    version="11.9.0",
+    version="11.9.1",
     description="A flexible Python Kconfig implementation",
 
     # Make sure that README.rst decodes on Python 3 in environments that use
