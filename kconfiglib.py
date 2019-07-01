a65c52be7678cdf70ff6ab2a7f79d40dbed90e4d
Ulf Magnusson <ulfalizer@gmail.com>
2019-07-01 22:25:26 +0200

    Remove redundant comment

--- kconfiglib.py
+++ kconfiglib.py
@@ -5894,7 +5894,6 @@ def standard_sc_expr_str(sc):
             return '"{}"'.format(escape(sc.name))
         return sc.name
 
-    # Choice
     return "<choice {}>".format(sc.name) if sc.name else "<choice>"
 
 
